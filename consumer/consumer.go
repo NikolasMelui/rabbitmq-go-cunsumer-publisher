61f2eeefc1a8892b34d74bfee797f86350e80306
nikolasmelui <slak@samaradom.ru>
2020-08-09 21:22:47 +0400

    Add consumer.go main logic

--- consumer/consumer.go
+++ consumer/consumer.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"os"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/streadway/amqp"
 )
 
 var rabbitHost = os.Getenv("RABBIT_HOST")
@@ -16,5 +20,56 @@ type Data struct {
 }
 
 func main() {
+	amqpDialAddr := fmt.Sprintf("amqp://%s:%s@%s:%s/", rabbitUser, rabbitPassword, rabbitHost, rabbitPort)
+	conn, err := amqp.Dial(amqpDialAddr)
+	if err != nil {
+		log.Fatalf("%s: %s", "Failed to connect to RabbitMQ", err)
+	}
 
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("%s: %s", "Failed to open a channel", err)
+	}
+
+	q, err := ch.QueueDeclare(
+		"publisher",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		log.Fatalf("%s: %s", "Failed to declare a queue", err)
+	}
+
+	fmt.Println("Channel and queue established")
+
+	defer conn.Close()
+	defer ch.Close()
+
+	msgs, err := ch.Consume(
+		q.Name,
+		"",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		log.Fatalf("%s: %s", "Failed to register consumer", err)
+	}
+
+	forever := make(chan bool)
+
+	go func() {
+		for d := range msgs {
+			log.Printf("Received a message: %s", d.Body)
+
+			d.Ack(false)
+		}
+	}()
+
+	<-forever
 }
